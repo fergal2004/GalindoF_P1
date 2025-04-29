dc27f680ef2c2c66a41c622f6c90e287b65b8116
fernangal2004 <fernando.galindo@udla.edu.ec>
2025-04-29 12:11:00 -0500

    Cambio en la interfaz de usuario

--- obj/Debug/net9.0/GalindoFP1.AssemblyInfo.cs
+++ obj/Debug/net9.0/GalindoFP1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GalindoFP1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c1a0b048113fe1667463681adf037a122d816a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91a580db191a15db677dbc9a2911bf4d7ec98748")]
 [assembly: System.Reflection.AssemblyProductAttribute("GalindoFP1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GalindoFP1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
